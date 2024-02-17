e8b67d83ae3b6b35e450b812eb6fd949dd02184d
greatSumini <greatSumini@gmail.com>
2024-02-18 00:48:43 +0900

    fix(server-typeorm): 수정

--- packages/server/typeorm/src/module.ts
+++ packages/server/typeorm/src/module.ts
@@ -3,9 +3,8 @@ import {
   TypeOrmModule as _TypeOrmModule,
   TypeOrmModuleOptions,
   getDataSourceToken,
-  getCustomRepositoryToken,
 } from '@nestjs/typeorm';
-import { DataSource, DataSourceOptions } from 'typeorm';
+import { DataSource } from 'typeorm';
 
 import { Repository } from './interfaces';
 import { getEntityByRepository } from './utils';
@@ -13,20 +12,16 @@ import { getEntityByRepository } from './utils';
 import { findRootPath, loadDataSource } from './dataSource';
 import { findSrcPath } from './findSrcPath';
 
-function getProviders(
-  repositories: Repository[],
-  dataSource?: string | DataSource | DataSourceOptions
-): Provider[] {
+function getProviders(repositories: Repository[]): Provider[] {
   return repositories.map(repository => {
     const entity = getEntityByRepository(repository);
 
     return {
-      module: TypeOrmModule,
-      provide: getCustomRepositoryToken(repository),
+      provide: repository,
       useFactory: (dataSource: DataSource) => {
         return new repository(entity, dataSource.manager);
       },
-      inject: [getDataSourceToken(dataSource)],
+      inject: [getDataSourceToken()],
     } as Provider<any>;
   });
 }
@@ -55,14 +50,13 @@ export class TypeOrmModule {
     });
   }
 
-  static forFeature(
-    repositories: Repository[],
-    dataSource?: string | DataSource | DataSourceOptions
-  ): DynamicModule {
+  static forFeature(repositories: Repository[]): DynamicModule {
+    const providers = getProviders(repositories);
+
     return {
       module: TypeOrmModule,
-      providers: getProviders(repositories, dataSource),
-      exports: getProviders(repositories, dataSource),
+      providers,
+      exports: providers,
     };
   }
 }
