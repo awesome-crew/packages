a87068fd643f91a091eb434fb781576f379e60d0
greatSumini <greatSumini@gmail.com>
2024-02-18 01:15:02 +0900

    fix(server-typeorm): findSrcPath이 파일 경로 반환하는 오류 수정

--- packages/server/typeorm/src/findSrcPath.ts
+++ packages/server/typeorm/src/findSrcPath.ts
@@ -1,13 +1,14 @@
 import findUp from 'find-up';
+import path from 'path';
 
 export const SRC_SOURCE_FILE_NAMES = ['app.module.ts', 'app.module.js'];
 
 export async function findSrcPath(cwd = __dirname) {
-  const path = await findUp(SRC_SOURCE_FILE_NAMES, { cwd });
+  const _filePath = await findUp(SRC_SOURCE_FILE_NAMES, { cwd });
 
-  if (path == null) {
+  if (_filePath == null) {
     throw new Error(`@awesome-dev/server-typeorm ERROR::${SRC_SOURCE_FILE_NAMES} not found`);
   }
 
-  return path;
+  return path.dirname(_filePath);
 }
