c2c88c94478d8964b6207f961cf4bc2de6a34a03
greatSumini <greatSumini@gmail.com>
2024-02-27 00:06:07 +0900

    feat(typings@0.0.1): PartialDeep 추가

--- packages/common/typings/src/index.d.ts
+++ packages/common/typings/src/index.d.ts
@@ -1,4 +1,4 @@
 export * from './common';
 export * from './react';
 
-export { SetRequired, SetOptional, UnknownArray, UnknownRecord } from 'type-fest';
+export { SetRequired, SetOptional, UnknownArray, UnknownRecord, PartialDeep } from 'type-fest';
