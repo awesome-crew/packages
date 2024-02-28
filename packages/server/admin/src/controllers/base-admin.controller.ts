cf16ce89752e712099b7da8ba14e720862d56ef9
greatSumini <greatSumini@gmail.com>
2024-02-28 18:13:01 +0900

    enhance(server-admin@0.0.5): 기본 어드민 API들에서 relation을 전달받는다

--- packages/server/admin/src/controllers/base-admin.controller.ts
+++ packages/server/admin/src/controllers/base-admin.controller.ts
@@ -3,20 +3,21 @@ import {
   BaseService,
   ListOptions,
   ListOptionsQuery,
+  Relation,
 } from '@awesome-dev/server-common';
 import { PartialDeep } from '@awesome-dev/typings';
-import { Body, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Delete, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
 
 export abstract class BaseAdminController<Entity extends BaseIdEntity> {
   constructor(protected readonly service: BaseService<Entity>) {}
 
   @Get('/count')
-  count(@ListOptionsQuery() listOptions: ListOptions<Entity>) {
+  count(@ListOptionsQuery({ acceptRelations: true }) listOptions: ListOptions<Entity>) {
     return this.service.count(listOptions);
   }
 
   @Get()
-  list(@ListOptionsQuery() listOptions: ListOptions<Entity>) {
+  list(@ListOptionsQuery({ acceptRelations: true }) listOptions: ListOptions<Entity>) {
     return this.service.list(listOptions);
   }
 
@@ -26,8 +27,8 @@ export abstract class BaseAdminController<Entity extends BaseIdEntity> {
   }
 
   @Get('/:id')
-  read(@Param('id', ParseIntPipe) id: number) {
-    return this.service.get(id);
+  read(@Param('id', ParseIntPipe) id: number, @Query('relations') relations: Relation<Entity>[]) {
+    return this.service.get(id, relations);
   }
 
   @Patch('/:id')
