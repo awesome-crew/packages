97040f4905e60e591492b6fdd7346a986b411bcc
greatSumini <greatSumini@gmail.com>
2024-03-13 17:26:31 +0900

    enhance(typings@0.0.3): PropertyStringPath에 MaxDepth 추가
    - 무한대로 타입이 확장되는 것을 방지했습니다

--- packages/common/typings/src/PropertyStringPath.d.ts
+++ packages/common/typings/src/PropertyStringPath.d.ts
@@ -1,7 +1,38 @@
-export type PropertyStringPath<T, Prefix = ''> = {
+/**
+ * This type is used to generate a string path for a given object type.
+ * It will generate a string path for all properties of the object type.
+ * it only goes to a maximum depth of given levels. default,max is 10.
+ */
+export type PropertyStringPath<
+  T,
+  Prefix = '',
+  MaxDepth extends 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 = 10,
+> = {
   [K in keyof T]: T[K] extends Primitive | Array<any>
     ? `${string & Prefix}${string & K}`
-    :
-        | `${string & Prefix}${string & K}`
-        | PropertyStringPath<T[K], `${string & Prefix}${string & K}.`>;
+    : `${string & Prefix}${string & K}` | MaxDepth extends 0
+      ? never
+      : PropertyStringPath<
+          T[K],
+          `${string & Prefix}${string & K}.`,
+          MaxDepth extends 1
+            ? 0
+            : MaxDepth extends 2
+              ? 1
+              : MaxDepth extends 3
+                ? 2
+                : MaxDepth extends 4
+                  ? 3
+                  : MaxDepth extends 5
+                    ? 4
+                    : MaxDepth extends 6
+                      ? 5
+                      : MaxDepth extends 7
+                        ? 6
+                        : MaxDepth extends 8
+                          ? 7
+                          : MaxDepth extends 9
+                            ? 8
+                            : 9
+        >;
 }[keyof T];
